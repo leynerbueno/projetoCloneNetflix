7e82c24dd18244b5978d630ef2d3d5460411de91
Leyner Bueno <leyner_henrique@hotmail.com.br>
2021-03-31 17:56:36 -0300

    Finalizando páginas do meno inicial

--- js/setup.js
+++ js/setup.js
@@ -1,3 +1,34 @@
+// let series = [
+//     {
+//         nome: "Vikings",
+//         img: "../resourses/img/series/vikings.jpg"
+//     },
+//     {
+//         nome: "Breaking Bad",
+//         img: "../resourses/img/series/breakingbad.jpg"
+//     },
+//     {
+//         nome: "Cobra Kai",
+//         img: "../resourses/img/series/cobrakai.jpg"
+//     },
+//     {
+//         nome: "Jessica Jones",
+//         img: "../resourses/img/series/jessicajones.jpg"
+//     },
+// ]
+
+// $(window).on ("load",function () {
+//     let divContainer = document.getElementById("div-container");
+//     let divItem;
+//     series.forEach(serie => {
+//         console.log("rodou");
+//         divItem = document.createElement("div");
+//         divItem.setAttribute("class", "item");
+//         divItem.innerHTML = `<img class="box-video" src= ${serie.img}>`
+//         divContainer.appendChild(divItem);
+//     })
+// });
+
 
 //Carrousel
 $('.owl-carousel').owlCarousel({
